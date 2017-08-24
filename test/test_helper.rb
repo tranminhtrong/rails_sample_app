a05bc7a494a442cd3268d13ebde6aa34520c049e
tranminhtrong <trminhtrong@gmail.com>
2017-08-24 17:36:49 +0000

    Finish static pages

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,36 +1,6 @@
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
+require 'test_helper'
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  # Add more helper methods to be used by all tests here...
-  def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
-  end
-
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-    assert_select "title", "Home | #{@base_title}"
-  end
-
-  test "should get help" do
-    get static_pages_help_url
-    assert_response :success
-    assert_select "title", "Help | #{@base_title}"
-  end
-
-  test "should get about" do
-    get static_pages_about_url
-    assert_response :success
-    assert_select "title", "About | #{@base_title}"
-  end
   
-  test "should get contact" do
-    get static_pages_contact_url
-    assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
-  end
 end
